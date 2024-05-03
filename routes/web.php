5543d9368d944068ca794f764315774f9090ae5a
Jill Vina <jvina@my.bcit.ca>
2024-05-03 15:52:11 -0700

    Fix routing

--- routes/web.php
+++ routes/web.php
@@ -7,17 +7,29 @@ use Illuminate\Support\Facades\Log;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Home');
+    return redirect('/home');
 });
 
 $websitePages = \Illuminate\Support\Facades\File::allFiles(resource_path('js/Pages/Website'));
 foreach ($websitePages as $page) {
     $component = explode(".", $page->getRelativePathname())[0];
-    $name = Str::kebab($component);
+    $name = \Illuminate\Support\Str::kebab($component);
     Route::get($name, function() use ($component) {
         return Inertia::render("Website/$component");
     });
 }
+
+//Portal side must be authenticated
+Route::middleware(['auth', 'verified', 'web'])->group(function () {
+    $portalPages = \Illuminate\Support\Facades\File::allFiles(resource_path('js/Pages/Portal'));
+    foreach ($portalPages as $page) {
+        $component = explode(".", $page->getRelativePathname())[0];
+        $name = \Illuminate\Support\Str::kebab($component);
+        Route::get("/portal/$name", function() use ($component) {
+            return Inertia::render("Portal/$component");
+        });
+    }
+});
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
