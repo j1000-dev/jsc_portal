ed6c9919324546d5bdd7609fe52b91d59ee678d7
Jill Vina <jvina@my.bcit.ca>
2024-05-03 20:53:19 -0700

    Add laravel nova

---
+++ database/migrations/2024_05_04_033220_create_new_roles.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Spatie\Permission\Models\Role;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('roles', function (Blueprint $table) {
+            Role::create(['name' => 'admin']);
+            Role::create(['name' => 'coach']);
+            User::find(1)->assignRole('admin');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('roles', function (Blueprint $table) {
+            //
+        });
+    }
+};
