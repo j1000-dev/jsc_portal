12b5b706f7d96b2388b375f90c15b6182e717e8b
Jill Vina <jvina@my.bcit.ca>
2024-05-04 11:56:27 -0700

    Edit backend routing

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 Route::get('/', function () {
@@ -19,20 +19,44 @@ foreach ($websitePages as $page) {
     });
 }
 
-//Portal side must be authenticated
-Route::middleware(['auth', 'verified', 'web'])->group(function () {
+//Coaches
+Route::middleware(['auth', 'verified', 'web', 'role:coach'])->group(function () {
     $portalPages = \Illuminate\Support\Facades\File::allFiles(resource_path('js/Pages/Portal'));
     foreach ($portalPages as $page) {
         $component = explode(".", $page->getRelativePathname())[0];
         $name = \Illuminate\Support\Str::kebab($component);
         Route::get("/portal/$name", function() use ($component) {
             return Inertia::render("Portal/$component");
-        });
+        })->name('portal/'.$name);
     }
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
+//Admin
+Route::middleware(['auth', 'verified', 'web', 'role:admin'])->group(function () {
+    $portalPages = \Illuminate\Support\Facades\File::allFiles(resource_path('js/Pages/Admin'));
+    foreach ($portalPages as $page) {
+        $component = explode(".", $page->getRelativePathname())[0];
+        $name = \Illuminate\Support\Str::kebab($component);
+        Route::get("/admin/$name", function() use ($component) {
+            return Inertia::render("Admin/$component");
+        })->name('admin/'.$name);
+    }
+});
+
+/**
+ * REROUTING
+ * 1. Admin get rerouted to admin pages
+ * 2. Coaches get rerouted to coach pages
+ * 3. Coaches who have not filled out form get sent to sign up
+ **/
+Route::get('/dashboard', function (Request $request) {
+    if ($request->user()->hasRole('admin')) {
+        return redirect('/admin/dashboard');
+    } else if ($request->user()->hasRole('coach') && $request->user()->registered) {
+        return redirect('/portal/dashboard');
+    } else {
+        return redirect ('/portal/sign-up');
+    }
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
