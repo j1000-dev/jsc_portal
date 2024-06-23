6034ae4c70094f5abc4ba7b73d5bb3d41bb1cd63
Jill Vina <jvina@my.bcit.ca>
2024-06-23 16:18:16 -0700

    Add link

--- resources/js/Components/Banner.tsx
+++ resources/js/Components/Banner.tsx
@@ -2,9 +2,9 @@ export default function Banner() {
     return (
         <div className="flex flex-wrap items-center">
             <img src="/img/jsc-logo.jpg" width={125} />
-            <div className="font-bold text-6xl tracking-wide text-center">
+            <a href="/home" className="font-bold text-6xl tracking-wide text-center">
                 JEROME SPORTS CANADA
-            </div>
+            </a>
         </div>
     )
 }
